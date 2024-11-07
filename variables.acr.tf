1ba86ba23141ecb706397f7166662542768f2670
fblankendaal <fblankendaal@schubergphilis.com>
2024-11-07 14:47:50 +0100

    add more documentation

--- variables.acr.tf
+++ variables.acr.tf
@@ -72,7 +72,19 @@ Example Inputs:
 ```hcl
 
 module "acr" {
+  source = "somelocation"
 
+  acr = {
+    name                          = "myacr123"
+    resource_group_name           = "myrg"
+    location                      = "germanywestcentral"
+    admin_enabled                 = false
+    public_network_access_enabled = false
+    network_rule_bypass_option    = "AzureServices"
+  }
+  tags = {
+    "deploymentmodel" = "Terraform"
+  }
 }
 
 ```
