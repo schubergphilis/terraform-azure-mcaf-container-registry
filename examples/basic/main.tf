9423bc1c0df5a4855168e046cbc50964097bdfda
fblankendaal <fblankendaal@schubergphilis.com>
2024-11-07 14:15:48 +0100

    fix terraform fmt

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -6,10 +6,10 @@ module "acr" {
   source = "../.."
 
   acr = {
-    name                = "myacr"
-    resource_group_name = "myrg"
-    location            = "germanywestcentral"
-    admin_enabled       = false
+    name                          = "myacr"
+    resource_group_name           = "myrg"
+    location                      = "germanywestcentral"
+    admin_enabled                 = false
     public_network_access_enabled = false
     network_rule_bypass_option    = "AzureServices"
   }
