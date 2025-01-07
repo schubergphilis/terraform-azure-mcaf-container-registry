e34ac90796f6b1a163c85e9513e11112503ff65f
Dennis van der Meulen <dvandermeulen@schubergphilis.com>
2025-01-07 15:51:26 +0100

    remove test comments

--- locals.tf
+++ locals.tf
@@ -1,23 +1,4 @@
 locals {
-  # acr_managed_identities = {
-  #   system_assigned_user_assigned = (var.acr.managed_identities.system_assigned && (length(var.acr.managed_identities.user_assigned_resource_ids) > 0 || var.customer_managed_key.user_assigned_identity != null)) ? {
-  #     this = {
-  #       type                       = "SystemAssigned, UserAssigned"
-  #       user_assigned_resource_ids = setunion(var.acr.managed_identities.user_assigned_resource_ids, try([data.azurerm_user_assigned_identity.this[0].id], []))
-  #     }
-  #   } : null
-  #   system_assigned = var.acr.managed_identities.system_assigned ? {
-  #     this = {
-  #       type = "SystemAssigned"
-  #     }
-  #   } : null
-  #   user_assigned = (length(var.acr.managed_identities.user_assigned_resource_ids) > 0 || var.customer_managed_key.user_assigned_identity != null) ? {
-  #     this = {
-  #       type                       = "UserAssigned"
-  #       user_assigned_resource_ids = setunion(var.acr.managed_identities.user_assigned_resource_ids, try([data.azurerm_user_assigned_identity.this[0].id], []))
-  #     }
-  #   } : null
-  # }
   system_assigned_user_assigned = (var.acr.managed_identities.system_assigned && (length(var.acr.managed_identities.user_assigned_resource_ids) > 0 || var.customer_managed_key.user_assigned_identity != null)) ? {
     this = {
       type                       = "SystemAssigned, UserAssigned"
