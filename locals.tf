dc2997844afef8a81fca01bdacbf634808da9841
Dennis van der Meulen <dvandermeulen@schubergphilis.com>
2025-01-07 15:44:31 +0100

    test changed setup

--- locals.tf
+++ locals.tf
@@ -22,5 +22,14 @@ locals {
     type                       = "SystemAssigned, UserAssigned"
     user_assigned_resource_ids = setunion(var.acr.managed_identities.user_assigned_resource_ids, try([data.azurerm_user_assigned_identity.this[0].id], []))
   } : null
+  system_assigned = var.acr.managed_identities.system_assigned ? {
+    type                       = "SystemAssigned"
+    user_assigned_resource_ids = null
+  } : null
+  user_assigned = (length(var.acr.managed_identities.user_assigned_resource_ids) > 0 || var.customer_managed_key.user_assigned_identity != null) ? {
+    type                       = "UserAssigned"
+    user_assigned_resource_ids = setunion(var.acr.managed_identities.user_assigned_resource_ids, try([data.azurerm_user_assigned_identity.this[0].id], []))
+  } : null
+
   ordered_geo_replications = { for geo in var.acr.georeplications : geo.location => geo }
 }
