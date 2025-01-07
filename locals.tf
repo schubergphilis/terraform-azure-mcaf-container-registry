1cb499017a6d272f7962ccb7665bda1fbfa00fa6
Dennis van der Meulen <dvandermeulen@schubergphilis.com>
2025-01-07 15:42:20 +0100

    test changed setup

--- locals.tf
+++ locals.tf
@@ -1,22 +1,26 @@
 locals {
-  acr_managed_identities = {
-    system_assigned_user_assigned = (var.acr.managed_identities.system_assigned && (length(var.acr.managed_identities.user_assigned_resource_ids) > 0 || var.customer_managed_key.user_assigned_identity != null)) ? {
-      this = {
-        type                       = "SystemAssigned, UserAssigned"
-        user_assigned_resource_ids = setunion(var.acr.managed_identities.user_assigned_resource_ids, try([data.azurerm_user_assigned_identity.this[0].id], []))
-      }
-    } : null
-    system_assigned = var.acr.managed_identities.system_assigned ? {
-      this = {
-        type = "SystemAssigned"
-      }
-    } : null
-    user_assigned = (length(var.acr.managed_identities.user_assigned_resource_ids) > 0 || var.customer_managed_key.user_assigned_identity != null) ? {
-      this = {
-        type                       = "UserAssigned"
-        user_assigned_resource_ids = setunion(var.acr.managed_identities.user_assigned_resource_ids, try([data.azurerm_user_assigned_identity.this[0].id], []))
-      }
-    } : null
-  }
+  # acr_managed_identities = {
+  #   system_assigned_user_assigned = (var.acr.managed_identities.system_assigned && (length(var.acr.managed_identities.user_assigned_resource_ids) > 0 || var.customer_managed_key.user_assigned_identity != null)) ? {
+  #     this = {
+  #       type                       = "SystemAssigned, UserAssigned"
+  #       user_assigned_resource_ids = setunion(var.acr.managed_identities.user_assigned_resource_ids, try([data.azurerm_user_assigned_identity.this[0].id], []))
+  #     }
+  #   } : null
+  #   system_assigned = var.acr.managed_identities.system_assigned ? {
+  #     this = {
+  #       type = "SystemAssigned"
+  #     }
+  #   } : null
+  #   user_assigned = (length(var.acr.managed_identities.user_assigned_resource_ids) > 0 || var.customer_managed_key.user_assigned_identity != null) ? {
+  #     this = {
+  #       type                       = "UserAssigned"
+  #       user_assigned_resource_ids = setunion(var.acr.managed_identities.user_assigned_resource_ids, try([data.azurerm_user_assigned_identity.this[0].id], []))
+  #     }
+  #   } : null
+  # }
+  system_assigned_user_assigned = (var.acr.managed_identities.system_assigned && (length(var.acr.managed_identities.user_assigned_resource_ids) > 0 || var.customer_managed_key.user_assigned_identity != null)) ? {
+    type                       = "SystemAssigned, UserAssigned"
+    user_assigned_resource_ids = setunion(var.acr.managed_identities.user_assigned_resource_ids, try([data.azurerm_user_assigned_identity.this[0].id], []))
+  } : null
   ordered_geo_replications = { for geo in var.acr.georeplications : geo.location => geo }
 }
