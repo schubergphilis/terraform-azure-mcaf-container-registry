f364544a5e1516a0c617d0cb44370699a6e9c101
Dennis van der Meulen <dvandermeulen@schubergphilis.com>
2025-01-07 15:48:54 +0100

    fix iterator

--- locals.tf
+++ locals.tf
@@ -25,12 +25,16 @@ locals {
     }
   } : null
   system_assigned = var.acr.managed_identities.system_assigned ? {
-    type                       = "SystemAssigned"
-    user_assigned_resource_ids = null
+    this = {
+      type                       = "SystemAssigned"
+      user_assigned_resource_ids = null
+    }
   } : null
   user_assigned = (length(var.acr.managed_identities.user_assigned_resource_ids) > 0 || var.customer_managed_key.user_assigned_identity != null) ? {
-    type                       = "UserAssigned"
-    user_assigned_resource_ids = setunion(var.acr.managed_identities.user_assigned_resource_ids, try([data.azurerm_user_assigned_identity.this[0].id], []))
+    this = {
+      type                       = "UserAssigned"
+      user_assigned_resource_ids = setunion(var.acr.managed_identities.user_assigned_resource_ids, try([data.azurerm_user_assigned_identity.this[0].id], []))
+    }
   } : null
 
   ordered_geo_replications = { for geo in var.acr.georeplications : geo.location => geo }
