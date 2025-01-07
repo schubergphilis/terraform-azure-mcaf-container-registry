948e93c8fdc8d314f236782a54fa26a42547b266
Dennis van der Meulen <dvandermeulen@schubergphilis.com>
2025-01-07 15:56:43 +0100

    fix for no cmk info

--- locals.tf
+++ locals.tf
@@ -1,5 +1,5 @@
 locals {
-  system_assigned_user_assigned = (var.acr.managed_identities.system_assigned && (length(var.acr.managed_identities.user_assigned_resource_ids) > 0 || var.customer_managed_key.user_assigned_identity != null)) ? {
+  system_assigned_user_assigned = (var.acr.managed_identities.system_assigned && (length(var.acr.managed_identities.user_assigned_resource_ids) > 0 || var.customer_managed_key != null)) ? {
     this = {
       type                       = "SystemAssigned, UserAssigned"
       user_assigned_resource_ids = setunion(var.acr.managed_identities.user_assigned_resource_ids, try([data.azurerm_user_assigned_identity.this[0].id], []))
@@ -11,7 +11,7 @@ locals {
       user_assigned_resource_ids = null
     }
   } : null
-  user_assigned = (length(var.acr.managed_identities.user_assigned_resource_ids) > 0 || var.customer_managed_key.user_assigned_identity != null) ? {
+  user_assigned = (length(var.acr.managed_identities.user_assigned_resource_ids) > 0 || var.customer_managed_key != null) ? {
     this = {
       type                       = "UserAssigned"
       user_assigned_resource_ids = setunion(var.acr.managed_identities.user_assigned_resource_ids, try([data.azurerm_user_assigned_identity.this[0].id], []))
