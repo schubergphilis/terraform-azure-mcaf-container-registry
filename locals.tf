4cc809c22d292c57c88b79a62dd5e34ccac50ae8
Dennis van der Meulen <dvandermeulen@schubergphilis.com>
2025-01-07 15:20:44 +0100

    coalesce not try

--- locals.tf
+++ locals.tf
@@ -5,18 +5,18 @@ locals {
         type                       = "SystemAssigned, UserAssigned"
         user_assigned_resource_ids = setunion(var.acr.managed_identities.user_assigned_resource_ids, try([data.azurerm_user_assigned_identity.this[0].id], []))
       }
-    } : null
+    } : {}
     system_assigned = var.acr.managed_identities.system_assigned ? {
       this = {
         type = "SystemAssigned"
       }
-    } : null
+    } : {}
     user_assigned = (length(var.acr.managed_identities.user_assigned_resource_ids) > 0 || var.customer_managed_key.user_assigned_identity != null) ? {
       this = {
         type                       = "UserAssigned"
         user_assigned_resource_ids = setunion(var.acr.managed_identities.user_assigned_resource_ids, try([data.azurerm_user_assigned_identity.this[0].id], []))
       }
-    } : null
+    } : {}
   }
   ordered_geo_replications = { for geo in var.acr.georeplications : geo.location => geo }
 }
