2c73d8f569e3a5cd9896cfed0010c7973d7ed7a7
Dennis van der Meulen <dvandermeulen@schubergphilis.com>
2025-01-07 14:19:51 +0100

    cmk versionless id

--- locals.tf
+++ locals.tf
@@ -2,7 +2,7 @@ locals {
   acr_managed_identities = {
     system_assigned_user_assigned = (var.acr.managed_identities.system_assigned || length(var.acr.managed_identities.user_assigned_resource_ids) > 0) ? {
       this = {
-        type                       = var.acr.managed_identities.system_assigned && length(var.acr.managed_identities.user_assigned_resource_ids) > 0 ? "SystemAssigned, UserAssigned" : length(var.acr.managed_identities.user_assigned_resource_ids) > 0 ? "UserAssigned" : "SystemAssigned"
+        type                       = var.acr.managed_identities.system_assigned && (length(var.acr.managed_identities.user_assigned_resource_ids) || var.customer_managed_key.user_assigned_identity != null) > 0 ? "SystemAssigned, UserAssigned" : length(var.acr.managed_identities.user_assigned_resource_ids) > 0 ? "UserAssigned" : "SystemAssigned"
         user_assigned_resource_ids = var.acr.managed_identities.user_assigned_resource_ids
       }
     } : {}
--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ resource "azurerm_container_registry" "this" {
 
     content {
       identity_client_id = data.azurerm_user_assigned_identity.this[0].client_id
-      key_vault_key_id   = data.azurerm_key_vault_key.this[0].id
+      key_vault_key_id   = data.azurerm_key_vault_key.this[0].versionless_id
     }
   }
 
