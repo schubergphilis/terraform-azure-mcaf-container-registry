725b249c979ab951543ef92e1e2f17a6881bec58
Frank Muller <fmuller@schubergphilis.com>
2025-03-10 13:44:59 +0100

    this

--- main.tf
+++ main.tf
@@ -173,5 +173,5 @@ resource "azurerm_container_registry_cache_rule" "this" {
   container_registry_id = azurerm_container_registry.acr.id
   target_repo           = each.value.target_repo
   source_repo           = each.value.source_repo
-  credential_set_id     = each.value.credential_set_name != null ? "${azurerm_container_registry.acr.id}/credentialSets/${each.value.credential_set_name}" : null
+  credential_set_id     = each.value.credential_set_name != null ? "${azurerm_container_registry.this.id}/credentialSets/${each.value.credential_set_name}" : null
 }
